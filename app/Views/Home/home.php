89d90be42495666f8cbec0648ff96d521aeeef0a
Killian Deslandes <killian.deslandes53@gmail.com>
2024-12-02 17:32:18 +0100

    modif acceuil

--- app/Views/Home/home.php
+++ app/Views/Home/home.php
@@ -9,18 +9,18 @@
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://www.beautysuccess.fr/media/catalog/product/cache/02280392440d22bedc5c4ce4592badc4/4/3/4371782058-guerlain-l-homme-ideal-100ml-visuel_4.webp" class="d-block w-100" alt="Image 1" style="height: 400px; object-fit: cover;">
+                <img src="https://www.beautysuccess.fr/media/catalog/product/cache/02280392440d22bedc5c4ce4592badc4/4/3/4371782058-guerlain-l-homme-ideal-100ml-visuel_4.webp" class="d-block w-100" alt="Image 1">
             </div>
             <div class="carousel-item">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/3f1302d3-be16-4ce7-b177-b4421213d687.jpeg" class="d-block w-100" style="height: 400px; object-fit: cover;">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/3f1302d3-be16-4ce7-b177-b4421213d687.jpeg" class="d-block w-100">
             </div>
             <div class="carousel-item">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/77f77e8d-ceb1-4544-bcdd-b216e52c0d1a.jpeg" class="d-block w-100" style="height: 400px; object-fit: cover;">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/77f77e8d-ceb1-4544-bcdd-b216e52c0d1a.jpeg" class="d-block w-100">
             </div>
         </div>
     </div>
 
-    <!-- Section Catégories Vedettes -->
+    
     <div class="categories-vedette d-flex justify-content-around py-4">
         <?php
         for ($i = 1; $i < 5; $i++) {
@@ -29,7 +29,6 @@
         ?>
     </div>
 
-    <!-- Séparateur -->
     <hr class="trait-doree">
 
     <!-- Carrousel Produits -->
@@ -41,14 +40,14 @@
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/3f1302d3-be16-4ce7-b177-b4421213d687.jpeg" style="height: 300px;">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/7f0da4d2-b42d-4b0b-ac2f-a2bd78422b0b.jpeg" style="height: 300px;">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/e5972641-36bf-4b0a-aab4-62b7e5d5d17e.jpeg" style="height: 300px;">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/3f1302d3-be16-4ce7-b177-b4421213d687.jpeg">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/7f0da4d2-b42d-4b0b-ac2f-a2bd78422b0b.jpeg">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/e5972641-36bf-4b0a-aab4-62b7e5d5d17e.jpeg">
             </div>
             <div class="carousel-item">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/1200b8b3-272f-40e3-bc98-c928547e2ed1.jpeg" style="height: 300px;">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/9a09a44c-aa30-41e2-a9f7-ad6ad41a66a3.jpeg" style="height: 300px;">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/4f538ea5-703c-425d-88bc-3b01a935f18a.jpeg" style="height: 300px;">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/1200b8b3-272f-40e3-bc98-c928547e2ed1.jpeg">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/9a09a44c-aa30-41e2-a9f7-ad6ad41a66a3.jpeg">
+                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/4f538ea5-703c-425d-88bc-3b01a935f18a.jpeg">
             </div>
         </div>
 
@@ -71,16 +70,16 @@
 
     <!-- Section Produit Vedette -->
     <div class="text-center mt-5">
-        <h2>Notre produit vedette</h2>
+        <h2 class="texte-doree">Notre produit vedette</h2>
         <hr class="trait-doree">
 
-        <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/1200b8b3-272f-40e3-bc98-c928547e2ed1.jpeg" style="height: 300px;">
+        <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/1200b8b3-272f-40e3-bc98-c928547e2ed1.jpeg">
 
         <p>GOKUUU</p>
         <p>Lorem ipsum dolor est optio officiis vitae nisi ex llitia. Pariatur totam vitae repellendus assumenda possimus quae voluptatem aut.eque exercitationem culpa!</p>
-        <p>10€</p>
+        <p class="texte-doree">10€</p>
 
-        <button class="btn btn-primary">Ajouter au panier</button>
+        <button class="btn btn-black">Ajouter au panier<i class="fas fa-shopping-cart"></i></button>
 
         <!-- Contrôles de quantité -->
         <div class="container mt-5">
