45c4cc87be046e8668e66256960c295d2d1d0243
Killian Deslandes <killian.deslandes53@gmail.com>
2024-12-03 16:06:06 +0100

    reglage probleme lancement app

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -15,24 +15,27 @@ class Home extends BaseController
 
         $starProduct = $productModel->getStarProduct();
         $categories = $categoryModel->findAll();
-		$defaultCategoryId = $categories[0]['id_cat'];  
+        $defaultCategoryId = isset($categories[0]['id_cat']) ? $categories[0]['id_cat'] : null;  // Vérification si les catégories existent
 
         $productsByCategory = [];
         foreach ($categories as $category) {
-            $productsByCategory[$category['id_cat']] = $productModel
+            // Récupérer les produits pour chaque catégorie
+            $products = $productModel
                 ->select('product.*, image.img_path')
                 ->join('product_category', 'product.id_prod = product_category.id_prod')
                 ->join('image', 'image.id_img = product.id_img')
                 ->where('product_category.id_cat', $category['id_cat'])
                 ->findAll();
+
+            // Si des produits sont trouvés, les ajouter à la liste, sinon, mettre un tableau vide
+            $productsByCategory[$category['id_cat']] = !empty($products) ? $products : [];
         }
 
+        // Récupérer l'ID de la catégorie sélectionnée, ou la première catégorie par défaut
         $selectedCategoryId = isset($_GET['category_id']) ? (int) $_GET['category_id'] : $defaultCategoryId;
 
-        $selectedProducts = [];
-        if (isset($productsByCategory[$selectedCategoryId])) {
-            $selectedProducts = $productsByCategory[$selectedCategoryId];
-        }
+        // Si la catégorie sélectionnée a des produits, les afficher, sinon afficher un tableau vide
+        $selectedProducts = isset($productsByCategory[$selectedCategoryId]) ? $productsByCategory[$selectedCategoryId] : [];
 
         $data = [
             'pageTitle' => 'Accueil',
