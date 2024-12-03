1409d462a17fd8e49cb53c271975dd4a99b8c1b2
Clementin Ly <XtrayShow@yahoo.fr>
2024-12-03 15:33:32 +0100

    Page à propos

--- app/Views/commun/header.php
+++ app/Views/commun/header.php
@@ -8,6 +8,7 @@
 	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"> <!-- Pour l'icône "..." -->
+	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet"> <!-- Pour la police -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="/assets/js/app.js"></script>
 </head>
