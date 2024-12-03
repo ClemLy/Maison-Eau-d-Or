cef036752b9d66a18ac277c7b059cf90a71fb5f7
Killian Deslandes <killian.deslandes53@gmail.com>
2024-12-03 14:23:02 +0100

    produit vedette et categorie afficher via bado

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,17 +3,29 @@
 	namespace App\Controllers;
 
 	use App\Controllers\BaseController;
+	use App\Models\ProductModel;
+	use App\Models\CategoryModel;
 
 	class Home extends BaseController
 	{
 		public function index()
 		{
+		
+			$productModel  = new ProductModel();
+			$categoryModel = new CategoryModel();
+
+			$starPorduct = $productModel->getStarProduct();
+
+			$categories = $categoryModel->findAll();
+
 			$data = [
-				'pageTitle' => 'Accueil'
+				'pageTitle' => 'Accueil',
+				'starProduct' => $starPorduct,
+				'categories' => $categories
 			];
 
 			echo view('commun/header', $data);
-			echo view('Home/home');
+			echo view('Home/home', $data);
 			echo view('commun/footer');
 		}
 	}
--- app/Views/Home/home.php
+++ app/Views/Home/home.php
@@ -23,9 +23,12 @@
 
     <div class="categories-vedette d-flex justify-content-around py-4">
         <?php
-        for ($i = 1; $i < 5; $i++) {
-            echo '<h3>Catégorie ' . $i . '</h3>';
+        foreach ($categories as $categ) {
+            echo '<h3>'.$categ['cat_name'].'</h3>';
         }
+        // for ($i = 1; $i < 5; $i++) {
+        //     echo '<h3>Catégorie ' . $i . '</h3>';
+        // }
         ?>
     </div>
 
@@ -69,36 +72,45 @@
     </div>
 
     <!-- Section Produit Vedette -->
-    <div class="text-center mt-5">
-        <h2 class="texte-doree">Notre produit vedette</h2>
-        <hr class="trait-doree">
 
-        <div class="row align-items-center produit-vedette mt-4">
-            <!-- Colonne de l'image -->
-            <div class="col-md-6 d-flex justify-content-center div-img-vedette">
-                <img src="https://cdn.sumup.store/2/th640/5d5cee579871afa9fc5ea0b02c0bf18e/1200b8b3-272f-40e3-bc98-c928547e2ed1.jpeg" class="img-vedette" alt="Produit Vedette">
-            </div>
+    <?php if (isset($starProduct) && !empty($starProduct)) { ?>
+        <div class="text-center mt-5">
+            <h2 class="texte-doree">Notre produit vedette</h2>
+            <hr class="trait-doree">
 
-            <!-- Colonne texte -->
-            <div class="col-md-6 text-start vedette-texte ps-4">
-                <h3 class="fw-bold">GOKUUU</h3>
-                <p>Lorem ipsum dolor est optio officiis vitae nisi ex llitia. Pariatur totam vitae repellendus assumenda possimus quae voluptatem aut.eque exercitationem culpa!</p>
-                <p class="texte-doree fs-4 fw-bold">10€</p>
+            <div class="row align-items-center produit-vedette mt-4">
+                <!-- Colonne de l'image -->
+                <div class="col-md-6 d-flex justify-content-center div-img-vedette">
+                    <img src="<?= esc($starProduct['img_path']); ?>" class="img-vedette" alt="Produit Vedette">
+                </div>
 
-                <form action="<?= site_url('/panier/ajouter') ?>" method="post">
+                <!-- Colonne texte -->
+                <div class="col-md-6 text-start vedette-texte ps-4">
+                    <h3 class="fw-bold"><?= esc($starProduct['p_name']) ?></h3>
+                    <p><?= esc($starProduct['description']) ?></p>
+                    <p class="texte-doree fs-4 fw-bold"><?= esc($starProduct['p_price']) . '€' ?></p>
 
-                    <div class="d-flex align-items-center mt-3">
-                        <div class="input-group" style="width: 150px;">
-                            <button class="btn btn-outline-primary" type="button" id="decrement" onclick="updateValue(-1)">-</button>
-                            <input type="number" class="form-control text-center" id="numberInput" value="0" min="0" max="10" readonly>
-                            <button class="btn btn-outline-primary" type="button" id="increment" onclick="updateValue(1)">+</button>
+                    <form action="<?= site_url('/panier/ajouter') ?>" method="post">
+
+                        <div class="d-flex align-items-center mt-3">
+                            <div class="input-group" style="width: 150px;">
+                                <button class="btn btn-outline-primary" type="button" id="decrement" onclick="updateValue(-1)">-</button>
+                                <input type="number" class="form-control text-center" id="numberInput" value="0" min="0" max="10" readonly>
+                                <button class="btn btn-outline-primary" type="button" id="increment" onclick="updateValue(1)">+</button>
+                            </div>
                         </div>
-                    </div>
 
-                    <button class="btn btn-black mt-3">Ajouter au panier <i class="fas fa-shopping-cart"></i></button>
+                        <button class="btn btn-black mt-3">Ajouter au panier <i class="fas fa-shopping-cart"></i></button>
 
-                </form>
+                    </form>
 
+                </div>
             </div>
         </div>
-    </div>
+    <?php }
+            else
+            {
+                echo '<p class="alert alert-info">Aucun article en vedette</p>';
+            } 
+    ?>  
+</div>
