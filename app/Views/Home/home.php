c32307839972870fe0b26e4f51c329e211b3645f
Killian Deslandes <killian.deslandes53@gmail.com>
2024-12-10 10:15:41 +0100

    petite modif style home

--- app/Views/Home/home.php
+++ app/Views/Home/home.php
@@ -124,7 +124,7 @@
                     <p><?= esc($starProduct['description']) ?></p>
                     <p class="texte-doree fs-4 fw-bold"><?= esc($starProduct['p_price']) . '€' ?></p>
 
-                    <form action="<?= site_url('/panier/ajouter/' . $starProduct['id_prod']) ?>" method="post">
+                    <form class="d-flex flex-row justify-content-between flex-space-between" action="<?= site_url('/panier/ajouter/' . $starProduct['id_prod']) ?>" method="post">
                         <div class="d-flex align-items-center mt-3">
                             <div class="input-group" style="width: 150px;">
                                 <button class="btn btn-outline-dark btn-sm" type="button" id="decrement" onclick="updateValue(-1)">-</button>
