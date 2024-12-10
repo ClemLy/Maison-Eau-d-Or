1814983fac1fecc9c2d3d77860aa0efea9ed9bef
Kyllian LE BRETON <kyllian.lebreton27@gmail.com>
2024-12-10 20:03:31 +0100

    Fix droplist

--- app/Views/commun/header.php
+++ app/Views/commun/header.php
@@ -33,11 +33,13 @@
 						Boutique
 						</a>
 						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<?php if (isset($categories)): ?>
-								<?php foreach ($categories as $category): ?>
-									<li><a class="dropdown-item" href="<?= site_url("boutique/categorie/" .$category['cat_name']); ?>"><?php echo esc($category['cat_name']); ?></a></li>
-								<?php endforeach; ?>
-							<?php endif; ?>
+							<li>
+								<?php if (isset($categories)): ?>
+									<?php foreach ($categories as $category): ?>
+										<a class="dropdown-item" href="<?= site_url("boutique/categorie/" .$category['cat_name']); ?>"><?php echo esc($category['cat_name']); ?></a>
+									<?php endforeach; ?>
+								<?php endif; ?>
+							</li>
 						</ul>
 					</li>
 
@@ -54,13 +56,15 @@
 								Admin
 							</a>
 							<ul class="dropdown-menu" aria-labelledby="adminDropdown">
-								<li><a class="dropdown-item" href="<?= site_url('admin/produits'); ?>">Gestion Produits</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('admin/categories'); ?>">Gestion Catégories</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('admin/commandes'); ?>">Gestion Commandes</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('admin/blog'); ?>">Gestion Blog</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('admin/a-propos/modifier'); ?>">Modifier À Propos</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('admin/faq/modifier'); ?>">Modifier FAQ</a></li>
-                                <li><a class="dropdown-item" href="<?= site_url('admin/gestionImage'); ?>">Gestion Images</a></li>
+								<li>
+									<a class="dropdown-item" href="<?= site_url('admin/produits'); ?>">Gestion Produits	</a>
+									<a class="dropdown-item" href="<?= site_url('admin/categories'); ?>">Gestion Catégories</a>
+									<a class="dropdown-item" href="<?= site_url('admin/commandes'); ?>">Gestion Commandes</a>
+									<a class="dropdown-item" href="<?= site_url('admin/blog'); ?>">Gestion Blog</a>
+									<a class="dropdown-item" href="<?= site_url('admin/a-propos/modifier'); ?>">Modifier À Propos</a>
+									<a class="dropdown-item" href="<?= site_url('admin/faq/modifier'); ?>">Modifier FAQ</a>
+									<a class="dropdown-item" href="<?= site_url('admin/gestionImage'); ?>">Gestion Images</a>
+								</li>
 							</ul>
 						</li>
 					<?php endif; ?>
@@ -71,10 +75,12 @@
 								Mon Compte
 							</a>
 							<ul class="dropdown-menu" aria-labelledby="accountDropdown">
-								<li><a class="dropdown-item" href="<?= site_url('account/historique'); ?>">Historique</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('account/update'); ?>">Modifier</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('account/delete'); ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.');">Supprimer</a></li>
-								<li><a class="dropdown-item" href="<?= site_url('logout'); ?>">Se Déconnecter</a></li>
+								<li>
+									<a class="dropdown-item" href="<?= site_url('account/historique'); ?>">Historique</a>
+									<a class="dropdown-item" href="<?= site_url('account/update'); ?>">Modifier</a></li>
+									<a class="dropdown-item" href="<?= site_url('account/delete'); ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.');">Supprimer</a></li>
+									<a class="dropdown-item" href="<?= site_url('logout'); ?>">Se Déconnecter</a>
+								</li>
 							</ul>
 						</li>
 					<?php else: ?>
